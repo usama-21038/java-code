6de3a9f20b12e1835db64d1881966f1063ba5802
shihab-38 <shihabhasib777@gmail.com>
2024-06-09 22:50:44 +0600

    Create format_specifier.java

---
+++ format_specifier.java
@@ -0,0 +1,16 @@
+class format_specifier{
+    public static void main(String[] args){
+char c='a';
+short s=32677;
+int i=126587;
+float f=10.2f;
+double d=10.3;
+
+System.out.printf("character=%c\n",c);
+System.out.printf("Short=%d\n",s);
+System.out.printf("Int=%d\n",i);
+System.out.printf("Float=%f\n",f);
+System.out.printf("Double=%f\n",d);
+
+       }
+}
