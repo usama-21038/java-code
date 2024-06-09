ff4eacd260583bbf2c0984f98e95ab4c80d90fd5
shihab-38 <shihabhasib777@gmail.com>
2024-06-09 22:21:36 +0600

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,17 @@
+class vairable{
+    public static void main(String args[]){
+      boolean b=true;
+      char c='a';
+      short s=32677;
+      int i=1234;
+      float f=10.2f;
+      double d=10.3;
+      
+      System.out.println("B="+b);
+      System.out.println("c="+c);
+      System.out.println("short="+s);
+      System.out.println("Int="+i);
+      System.out.println("Float="+f);
+      System.out.println("Double="+d);
+       }
+}
